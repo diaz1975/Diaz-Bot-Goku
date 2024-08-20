504024d750b2ad612e777577bd97def20430d585
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-20 15:03:15 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -151,12 +151,11 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     
-let urls = [
-"https://telegra.ph/file/8376e8dff8817ebbb18eb.mp4", 
-"https://telegra.ph/file/8376e8dff8817ebbb18eb.mp4" 
-];
-let gifUrl = urls[Math.floor(Math.random() * urls.length)];
-await conn.sendMessage(m.chat, {video: {url: gifUrl}, gifPlayback: true, caption: text.trim(), mentions: [m.sender]}, {quoted: m});
+    let img = await (await fetch(`https://telegra.ph/file/d535430793cd5cb177c58.mp4`)).buffer()
+    await m.react('⭐')
+   // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
+    await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
+   //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
 
 
   } catch (e) {
