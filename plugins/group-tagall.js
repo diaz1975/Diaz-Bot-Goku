ddeeeead141996d5b307bc36377bd37882b3c4fa
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-08-20 10:55:48 -0500

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -10,9 +10,9 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `${pesan}`;
-  let teks = `*Son - Goku✨*\n\n *Integrantes :  ${participants.length}* ${oi}\n\n┌──⭓ Ya revivan\n`;
+  let teks = `*Diaz - Bot🥷🏻*\n\n *Integrantes :  ${participants.length}* ${oi}\n\n┌──⭓ Ya revivan\n`;
   for (const mem of participants) {
-    teks += `🐉 @${mem.id.split('@')[0]}\n`;
+    teks += `🥷🏻 @${mem.id.split('@')[0]}\n`;
   }
   teks += `└───────⭓
 
