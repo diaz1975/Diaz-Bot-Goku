42ee510ee77929a34c48fa00de24c7fce22fcbaa
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-08-19 19:53:23 -0500

    Update main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -4,9 +4,9 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let img = await (await fetch(`https://telegra.ph/file/0dbf7b27644e8dfa8baa4.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = `*Hola!, te invito a formar parte de la comunidad de ░G░o░k░u░-░B░l░a░c░k░*
+let txt = `*Hola!, te invito a formar parte de la comunidad de 🥷🏻Diaz-Bot🥷🏻*
 
->  Enlace Grupo 🌀SonGoku🌀
+>  Enlace Grupo 🥷🏻Diaz-Bot🥷🏻
 
 *👑* ${group}
 
