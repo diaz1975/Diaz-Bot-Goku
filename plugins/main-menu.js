0fdd24a658cc0a2a6c12a7696d6ee3392c5a66a4
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-08-20 15:21:34 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -147,7 +147,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     
-    let img = await (await fetch(`https://telegra.ph/file/d535430793cd5cb177c58.mp4`)).buffer()
+    let img = await (await fetch(`https://telegra.ph/file/810720373080d33ace9b1.mp4`)).buffer()
     await m.react('⭐')
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
